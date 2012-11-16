788db51fbb012853996aade81ce3fa039e78f628
Djiedjom Aniakou <pacom20003@hotmail.com>
2012-11-16 01:55:14 -0500

    Created Send message view

--- src/com/hackrgt/katanalocate/MainActivity.java
+++ src/com/hackrgt/katanalocate/MainActivity.java
@@ -1,11 +1,9 @@
 package com.hackrgt.katanalocate;
 
-import com.example.katanalocate.R;
+import com.hackrgt.katanalocate.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
