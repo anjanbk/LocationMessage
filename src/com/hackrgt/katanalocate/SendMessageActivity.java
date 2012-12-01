1f41c53a5c74f737d218f4296a767eba84d7ae74
Djiedjom Aniakou <pacom20003@hotmail.com>
2012-11-30 22:45:26 -0500

    Save msg info when switching b/w activities

--- src/com/hackrgt/katanalocate/SendMessageActivity.java
+++ src/com/hackrgt/katanalocate/SendMessageActivity.java
@@ -4,12 +4,16 @@ import java.util.Calendar;
 import java.util.Formatter;
 
 import com.hackrgt.katanalocate.friendslist.FriendListActivity;
+import com.hackrgt.katanalocate.helper.ObjectSaver;
 
 import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,12 +28,23 @@ public class SendMessageActivity extends Activity implements OnClickListener, On
 	private TimePickerDialog timePicker;
 	private Calendar calendar;
 	
-	private String msgRecipientName, msgTime, msgLocation, msgSubject, msgBody, msgRecipientId;
+	private SharedPreferences prefs;
+	private Editor editor;
+	private String msgRecipientName, msgTime, msgLocation, msgRecipientId;
+	
+	private final String STORED_TIME_FLAG = "timeSelected";
+	private final String STORED_RECIEPIENT_FLAG = "userSelected";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_message_view);
+        
+        ObjectSaver.setActivity(this);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        editor = prefs.edit();
+        //editor.putBoolean("timeSelected", false);
+        editor.commit();
         
         //messageTo = (EditText) findViewById(R.id.messageTo);
         messageSubject = (EditText) findViewById(R.id.messageSubject);
@@ -55,15 +70,39 @@ public class SendMessageActivity extends Activity implements OnClickListener, On
         
         //Extract variables if passed in
         Bundle extras = getIntent().getExtras();
-        if (extras != null) { 	
+        if (extras != null) {
         	msgLocation = extras.getString("location_name");
         	if (msgLocation != null)
         		chooseLocation.setText("Choose Location: ("+msgLocation+")");
         	
         	msgRecipientId = extras.getString("user_id");
         	msgRecipientName = extras.getString("user_name");
-        	if (msgRecipientName != null)
+        	if (msgRecipientId != null) {
         		chooseRecepient.setText("To: ("+msgRecipientName+")");
+        		
+        		editor.putBoolean(STORED_RECIEPIENT_FLAG, true);
+        		editor.putString("userId", msgRecipientId);
+        		editor.putString("userName", msgRecipientName);
+                editor.commit();
+        	}
+        	//Set user again if previously set
+        	else if (prefs.getBoolean(STORED_RECIEPIENT_FLAG, false)) {
+        		msgRecipientId = prefs.getString("userId", null);
+        		msgRecipientName = prefs.getString("userName", null);
+        		if (msgRecipientId != null)
+            		chooseRecepient.setText("To: ("+msgRecipientName+")");
+        	}
+        }
+        
+        //Set time again if previously set
+        if (prefs.getBoolean(STORED_TIME_FLAG, false)) {
+        	int hr = prefs.getInt("messageTimeHour", -1);
+        	int min = prefs.getInt("messageTimeMinute", -1);
+        	if (hr != -1 && min != -1) {
+	        	calendar.set(Calendar.HOUR_OF_DAY, hr);
+	        	calendar.set(Calendar.MINUTE, min);
+	        	updateTime();
+        	}
         }
     }
 
@@ -90,7 +129,9 @@ public class SendMessageActivity extends Activity implements OnClickListener, On
 		else if (viewId == submitButton.getId()) {
 			String subject = messageSubject.getText().toString();
 			String body = messageBody.getText().toString();
+			sendMessage(msgRecipientId, subject, msgLocation, msgTime, body);
 			
+			clearStoreVarFlags();
 			Intent activity = new Intent(this, MainActivity.class);
 			startActivity(activity);
 		}
@@ -99,7 +140,18 @@ public class SendMessageActivity extends Activity implements OnClickListener, On
 	public void onTimeSet(TimePicker view, int hour, int minute) {
 		calendar.set(Calendar.HOUR_OF_DAY, hour);
     	calendar.set(Calendar.MINUTE, minute);
+    	
+    	//Flag for checking if time was selected
+        editor.putBoolean(STORED_TIME_FLAG, true);
+        editor.putInt("messageTimeHour", hour);
+        editor.putInt("messageTimeMinute", minute);
+        editor.commit();
     	updateTime();
+	}
+	
+	private void sendMessage(String msgRecipientId, String msgSubject, String msgLocation, String msgTime, String msgBody) {
+		//TODO: Send message to server
+		
 	}
 	
 	private void updateTime() {
@@ -115,5 +167,11 @@ public class SendMessageActivity extends Activity implements OnClickListener, On
         else
         	am_pm = "PM";
     	return fmt.toString()+" "+am_pm;
+	}
+	
+	private void clearStoreVarFlags() {
+		editor.putBoolean(STORED_TIME_FLAG, false);
+		editor.putBoolean(STORED_RECIEPIENT_FLAG, false);
+		editor.commit();
 	}
 }
