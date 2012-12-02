e2a9f6fc2643320a7bd8910b5938c86b514af3e3
Nathan Hurley <natehur77@gmail.com>
2012-12-02 16:47:54 -0500

    Updated map to drop pin

--- src/com/hackrgt/katanalocate/MapItemizedOverlay.java
+++ src/com/hackrgt/katanalocate/MapItemizedOverlay.java
@@ -8,10 +8,12 @@ import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class MapItemizedOverlay extends ItemizedOverlay<OverlayItem> {
@@ -29,26 +31,43 @@ public class MapItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	  mContext = context;
 	}
 	
+	 @Override
+	 public boolean onTouchEvent(MotionEvent event, MapView mapView) {
+		 final int action = event.getAction();
+		 boolean result = false;
+		 if (action == MotionEvent.ACTION_DOWN) {
+			 GeoPoint p = mapView.getProjection().fromPixels(
+                     (int) event.getX(),
+                     (int) event.getY());
+			 mOverlays.add(new OverlayItem(p, "", ""));
+			 populate();
+			 result = true;
+		 }
+		 return(result || super.onTouchEvent(event, mapView));
+	 }
+	
 	@Override
 	protected boolean onTap(int index) {
 	  OverlayItem item = mOverlays.get(index);
-	  Toast.makeText(mContext, item.getTitle()+" selected...", Toast.LENGTH_SHORT).show();
-	  
-	  /*AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
-	  dialog.show();*/
-	  
-	  //Store as selected location
-	  GeoPoint p = item.getPoint();
-	  Location loc = new Location(LocationManager.GPS_PROVIDER);
-	  if (loc != null) {
-		  loc.setLatitude(p.getLatitudeE6()/1000000F);
-		  loc.setLongitude(p.getLongitudeE6()/1000000F);
-		  selectedLocation = loc;
-	  }
+//	  Toast.makeText(mContext, item.getTitle()+" selected...", Toast.LENGTH_SHORT).show();
+//	  
+//	  /*AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+//	  dialog.setTitle(item.getTitle());
+//	  dialog.setMessage(item.getSnippet());
+//	  dialog.show();*/
+//	  
+//	  //Store as selected location
+//	  GeoPoint p = item.getPoint();
+//	  Location loc = new Location(LocationManager.GPS_PROVIDER);
+//	  if (loc != null) {
+//		  loc.setLatitude(p.getLatitudeE6()/1000000F);
+//		  loc.setLongitude(p.getLongitudeE6()/1000000F);
+//		  selectedLocation = loc;
+//	  }
 	  return true;
 	}
+	
+	
 
 	public void addOverlay(OverlayItem overlay) {
 	    mOverlays.add(overlay);
@@ -68,5 +87,6 @@ public class MapItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	public Location getSelectedLocation() {
 		return selectedLocation;
 	}
+	
 
 }
--- src/com/hackrgt/katanalocate/MapLocateActivity.java
+++ src/com/hackrgt/katanalocate/MapLocateActivity.java
@@ -5,27 +5,34 @@ import java.util.List;
 import java.util.Locale;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
 public class MapLocateActivity extends MapActivity {
+    
 	private EditText mapSearchBox;
 	private MapView mapView;
 	private MyLocationOverlay myLocOverlay;
@@ -46,60 +53,69 @@ public class MapLocateActivity extends MapActivity {
 	}
 	
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_view);
-        
-        mapView = (MapView)findViewById(R.id.mapview);
-        
-        // Center location on current position
-        List<Overlay> overlays = mapView.getOverlays();
-        myLocOverlay = new MyLocationOverlay(this, mapView);
-        
-        /**Changes*******************/
-        Drawable drawable = this.getResources().getDrawable(R.drawable.map_marker_icon);
-        itemizedOverlay = new MapItemizedOverlay(drawable, this);
-        
-        GeoPoint myLoc = myLocOverlay.getMyLocation();
-        myLocOverlay.enableMyLocation();
-        //myLocOverlay.onTouchEvent(this, mapView);
-        mapView.getOverlays().add(myLocOverlay);
-        
-        if (myLoc != null)
-        	System.out.println(myLoc.getLatitudeE6()+", "+myLoc.getLongitudeE6());
-        else
-        	System.out.println("My Loc is null!");
-        
-        /**End of Changes*******************/
-        
-        overlays.add(myLocOverlay);
-        
-        myLocOverlay.runOnFirstFix(new Runnable() {
-        	public void run() {
-        		mapView.getController().animateTo(myLocOverlay.getMyLocation());
-        		mapView.getController().setZoom(16);
-        	}
-        });
-        
-        mapSearchBox  = (EditText)findViewById(R.id.search_box);
-        mapSearchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-			
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if (actionId == EditorInfo.IME_ACTION_SEARCH ||
-	                    actionId == EditorInfo.IME_ACTION_DONE ||
-	                    actionId == EditorInfo.IME_ACTION_GO ||
-	                    event.getAction() == KeyEvent.ACTION_DOWN &&
-	                    event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-	                imm.hideSoftInputFromWindow(mapSearchBox.getWindowToken(), 0);
-
-	                new SearchClicked(mapSearchBox.getText().toString()).execute();
-	                mapSearchBox.setText("", TextView.BufferType.EDITABLE);
-	                return true;
-				}
-				return false;
-			}
-		});
+		 super.onCreate(savedInstanceState);
+	     setContentView(R.layout.map_view);
+	     MapView mapView = (MapView) findViewById(R.id.mapview);
+	     mapView.setBuiltInZoomControls(true);
+	     Drawable drawable = this.getResources().getDrawable(R.drawable.pin);
+	     mapView.getOverlays().add(new MapItemizedOverlay(drawable));
+	     myLocOverlay=new MyLocationOverlay(this, mapView);
+	     mapView.getOverlays().add(myLocOverlay);
+	     
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.map_view);
+//        
+//        mapView = (MapView)findViewById(R.id.mapview);
+//        mapView.setBuiltInZoomControls(true);
+//        
+//        // Center location on current position
+//        List<Overlay> overlays = mapView.getOverlays();
+//        myLocOverlay = new MyLocationOverlay(this, mapView);
+//        
+//        /**Changes*******************/
+//        itemizedOverlay = new MapItemizedOverlay(drawable, this);
+//        
+//        GeoPoint myLoc = myLocOverlay.getMyLocation();
+//        myLocOverlay.enableMyLocation();
+//        //myLocOverlay.onTouchEvent(this, mapView);
+//        mapView.getOverlays().add(myLocOverlay);
+//        
+//        if (myLoc != null)
+//        	System.out.println(myLoc.getLatitudeE6()+", "+myLoc.getLongitudeE6());
+//        else
+//        	System.out.println("My Loc is null!");
+//        
+//        /**End of Changes*******************/
+//        
+//        overlays.add(myLocOverlay);
+//        
+//        myLocOverlay.runOnFirstFix(new Runnable() {
+//        	public void run() {
+//        		mapView.getController().animateTo(myLocOverlay.getMyLocation());
+//        		mapView.getController().setZoom(16);
+//        	}
+//        });
+//        
+//        mapSearchBox  = (EditText)findViewById(R.id.search_box);
+//        mapSearchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//			
+//			@Override
+//			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//				if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+//	                    actionId == EditorInfo.IME_ACTION_DONE ||
+//	                    actionId == EditorInfo.IME_ACTION_GO ||
+//	                    event.getAction() == KeyEvent.ACTION_DOWN &&
+//	                    event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+//					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//	                imm.hideSoftInputFromWindow(mapSearchBox.getWindowToken(), 0);
+//
+//	                new SearchClicked(mapSearchBox.getText().toString()).execute();
+//	                mapSearchBox.setText("", TextView.BufferType.EDITABLE);
+//	                return true;
+//				}
+//				return false;
+//			}
+//		});
     }
 	
 	private class SearchClicked extends AsyncTask<Void, Void, Boolean> {
@@ -135,5 +151,6 @@ public class MapLocateActivity extends MapActivity {
             }
             return true;
         }
-	}
+        
+        }
 }
